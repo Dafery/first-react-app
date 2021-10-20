4a53436af98b8f59f91af56dded73105464e2d0b
Maria Abramovich <mashkala2@gmail.com>
2021-10-20 21:23:10 +0300

    We take out the data into index.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,9 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-  let posts = [
-    { id: 1, message: "Hi, how are you?", likesCount: 12 },
-    { id: 2, message: "It's my first post", likesCount: 5 },
-    { id: 3, message: "Blabla", likesCount: 5 },
-    { id: 4, message: "Dada", likesCount: 5 },
-  ];
-
-  let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
+  let postsElements = props.posts.map( p => (
+    <Post message={p.message} likesCount={p.likesCount} />
+  ));
 
   return (
     <div className={s.postsBlock}>
@@ -23,9 +18,7 @@ const MyPosts = (props) => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={s.posts}>
-        {postsElements}
-      </div>
+      <div className={s.posts}>{postsElements}</div>
     </div>
   );
 };
