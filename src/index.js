d44740c699112f6db2908431b1a0dce56907f77a
Maria Abramovich <mashkala2@gmail.com>
2021-11-09 14:50:42 +0300

    33. adding a post to the wall (beginnings of the FLUX concept)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
 import state from "./redux/state.js";
 import reportWebVitals from "./reportWebVitals";
-import {addPost} from "./redux/state"
+import { rerenderEntireTree } from "./render";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App state={state} addPost={addPost} />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+rerenderEntireTree(state);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
