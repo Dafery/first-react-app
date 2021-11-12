c03390a136cdee78325f8cb00db0d0ef8f2f403b
Maria Abramovich <mashkala2@gmail.com>
2021-11-12 19:59:56 +0300

    34. removal of the brain (FLUX-circulation for each symbol)

--- src/redux/state.js
+++ src/redux/state.js
@@ -8,6 +8,7 @@ let state = {
       { id: 3, message: "Blabla", likesCount: 5 },
       { id: 4, message: "Dada", likesCount: 5 },
     ],
+    newPostText: 'it-kamasutra.com'
   },
   dialogsPage: {
     dialogs: [
@@ -28,13 +29,21 @@ let state = {
   },
 };
 
-export let addPost = (postMessage) => {
+window.state = state;
+
+export let addPost = () => {
   let newPost = {
     id: 5,
-    message: postMessage,
+    message: state.profilePage.newPostText,
     likesCount: 0,
   };
   state.profilePage.posts.push(newPost);
+  state.profilePage.newPostText = '';
+  rerenderEntireTree(state);
+};
+
+export let updateNewPostText = (newText) => {
+  state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
 };
 
